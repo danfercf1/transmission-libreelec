36670832cb7a682303264afd209f047e3b9418dd
Daniel Candia <danfercf@gmail.com>
2017-06-24 19:11:48 -0400

    add current dir

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 /storage/.kodi/addons/service.system.docker/bin/docker pull libreelec/transmission:latest
-systemctl enable transmission.service
+systemctl enable $PWD/transmission.service
 systemctl start transmission
