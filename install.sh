18f44eb6dcb6d6c56c5e92a951f561325350b380
danfercf1 <danfercf@gmail.com>
2017-06-24 22:52:12 -0400

    update transmission image

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-/storage/.kodi/addons/service.system.docker/bin/docker pull libreelec/transmission:latest
+/storage/.kodi/addons/service.system.docker/bin/docker pull libreelecarm/transmission:latest
 systemctl enable $PWD/transmission.service
 systemctl start transmission
